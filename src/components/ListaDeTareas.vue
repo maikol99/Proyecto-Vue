92e03b08ffb45ec4dc8cc20e2f043517860a0f27
maikol99 <alegremaikol6@gmail.com>
2024-09-03 02:31:19 -0300

    cree componente Lista De Tareas, Borre componentes AutoGenerados, Cree rutas

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tarea, i) in tareas" :key="index">
+                {{ tarea }}
+                <button @click="borrarTarea">Borrar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const tareas= ref([])
+const nuevaTarea = ref('');
+const addTarea = () =>{
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+
+const borrarTarea = (i) =>{
+    tareas.value.splice(i,1);
+}
+
+
+</script>
+  
+<style scoped>
+
+</style>
+  
+  
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="sidebar">
+    <h2>Barra Lateral</h2>
+    <ul>
+        <li><RouterLink to="/contador">Contador</RouterLink></li>
+        <li><RouterLink to="/lista-de-tareas">Lista De Tareas</RouterLink></li>
+    </ul>
+    
+    
+</div>
+
+
+
+
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style>
+.sidebar {
+    width: 200px;
+    background-color: #f0f0f0;
+    padding: 20px;
+}
+</style>
